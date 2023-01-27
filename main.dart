3aa6122a84ed7a138d15d056ce38c025d39ec67a
Glory Malamsha <96989410+Glory-Mal@users.noreply.github.com>
2023-01-27 23:04:59 +0300

    i_am_glad
    Challenge of i am poor app.
    uploaded a i am glad output.png, gladcode1.png and diamong.png.
    2 last images are stored in images folder inside assets folder/directory.

--- main.dart
+++ main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false, //to remove debug banner
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: const Text('I am Rich'), //const was demanded for Text widget
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-            child: Image(
-          image: AssetImage('images/diamond.png'),
-        )),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+          title: const Text("I am glad"), backgroundColor: Colors.black87),
+      backgroundColor: Colors.grey,
+      body: const Center(
+        child: Image(image: AssetImage('images/gladcode1.png')),
       ),
     ),
-  );
+  ));
 }
+// debugShowCheckedModeBanner:false
