617b95d1fe90f07aa078d75ebb39c8b3d29f3857
Glory Malamsha <96989410+Glory-Mal@users.noreply.github.com>
2023-01-28 18:53:39 +0300

    mi_card_learning1
    App has a container widget with height, width, margin, padding and color properties. 
    has a child text.
    container is a single child layout widget

--- main.dart
+++ main.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-          title: const Text("I am glad"), backgroundColor: Colors.black87),
-      backgroundColor: Colors.grey,
-      body: const Center(
-        child: Image(image: AssetImage('images/gladcode1.png')),
-      ),
-    ),
-  ));
+  runApp(
+    MyApp()
+  );
 }
-// debugShowCheckedModeBanner:false
+
+class MyApp extends StatelessWidget {
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(
+          height: 100.0,
+          width: 100.0,
+          margin: EdgeInsets.all(20.0),//for outside container
+          padding: EdgeInsets.all(30.0),//for inside container from the child
+          color:Colors.white,
+          child:Text('Hello')
+        ),
+      ),
+    );
+  }
+}
+
+// to use safe area for phone
+// body: SafeArea(
+// child: Container(
+// color:Colors.white,
+// child:Text('Hello')
+// ),
+// ),
